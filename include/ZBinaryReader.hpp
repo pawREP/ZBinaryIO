ca8bcbd9e637c2f0487332919fe7910476b26833
pawREP <pawREP@users.noreply.github.com>
2020-05-02 13:30:48 +0200

    Init

---
+++ include/ZBinaryReader.hpp
@@ -0,0 +1,435 @@
+#include <algorithm>
+#include <assert.h>
+#include <cstring>
+#include <fstream>
+#include <memory>
+#include <vector>
+#include <cstdint>
+#include <filesystem>
+
+// Check if system is little endian
+static_assert(static_cast<const uint8_t&>(0x0B00B135) == 0x35);
+
+namespace ZBinaryReader {
+
+class ISource {
+public:
+    virtual void read(char* dst, int64_t len) = 0;
+    virtual void peek(char* dst, int64_t len) const = 0;
+    virtual void seek(int64_t offset) = 0;
+    virtual int64_t tell() const noexcept = 0;
+    virtual int64_t size() const noexcept = 0;
+
+    virtual ~ISource(){};
+};
+
+class FileSource : public ISource {
+private:
+    int64_t size_;
+    mutable std::ifstream ifs;
+
+public:
+    explicit FileSource(const std::string& path);
+    explicit FileSource(const char* path);
+    explicit FileSource(const std::filesystem::path& path);
+
+    ~FileSource();
+
+    void read(char* dst, int64_t len) override;
+    void peek(char* dst, int64_t len) const override;
+    void seek(int64_t offset) override final;
+    [[nodiscard]] int64_t tell() const noexcept override final;
+    [[nodiscard]] int64_t size() const noexcept override final;
+};
+
+class BufferSource : public ISource {
+private:
+    std::unique_ptr<char[]> ownedBuffer;
+
+    const char* buffer;
+    const int64_t bufferSize;
+    int64_t cur;
+
+public:
+    // Non owning constructor
+    BufferSource(const char* data, int64_t data_size);
+    // Owning constructor
+    BufferSource(std::unique_ptr<char[]> data, int64_t data_size);
+
+    void read(char* dst, int64_t len) override;
+    void peek(char* dst, int64_t len) const override;
+    void seek(int64_t offset) override final;
+    [[nodiscard]] int64_t tell() const noexcept override final;
+    [[nodiscard]] int64_t size() const noexcept override final;
+};
+
+class BinaryReader;
+
+template <typename Source>
+class CoverageTrackingSource : public Source {
+    static_assert(std::is_base_of_v<ISource, Source>);
+
+    std::vector<char> accessPattern; // TODO: replace with more space efficient, interval based, access pattern tracking
+
+    bool completeCoverageInternal() const;
+
+public:
+    template <typename... Args>
+    CoverageTrackingSource(Args&&... args);
+
+    void read(char* dst, int64_t len) override;
+
+    [[nodiscard]] static bool completeCoverage(const BinaryReader* br);
+};
+
+class BinaryReader {
+    std::unique_ptr<ISource> source;
+
+public:
+    enum class Endianness { LE, BE };
+
+    BinaryReader(BinaryReader& br) = delete;
+    BinaryReader(BinaryReader&& br) = delete;
+
+    //File Source constructors
+    BinaryReader(const std::filesystem::path& path);
+    BinaryReader(const std::string& path);
+    BinaryReader(const char* path);
+
+    // Buffer Source constructors
+    BinaryReader(const char* data, int64_t data_size);
+    BinaryReader(std::unique_ptr<char[]> data, int64_t data_size);
+    BinaryReader(std::unique_ptr<ISource> source);
+
+    BinaryReader& operator=(const BinaryReader& br) = delete;
+    BinaryReader& operator=(BinaryReader&& br) = delete;
+
+    [[nodiscard]] int64_t tell() const noexcept;
+    void seek(int64_t pos);
+    [[nodiscard]] int64_t size() const noexcept;
+
+    template <typename T, Endianness en = Endianness::LE>
+    void read(T* arr, int64_t len);
+
+    template <typename T, Endianness en = Endianness::LE>
+    T read();
+
+    template <typename T>
+    void sink(int64_t len);
+
+    template <typename T>
+    void sink();
+
+    template <typename T, Endianness en = Endianness::LE>
+    void peek(T* arr, int64_t len) const;
+
+    template <typename T, Endianness en = Endianness::LE>
+    T peek() const;
+
+    template <unsigned int len, Endianness en = Endianness::BE>
+    std::string readString();
+
+    template <Endianness en = Endianness::BE>
+    std::string readString(size_t charCount);
+
+    template <Endianness en = Endianness::BE>
+    std::string readCString();
+
+    template <unsigned int alignment = 0x10>
+    void align();
+
+    template <unsigned int alignment = 0x10>
+    void alignZeroPad();
+
+    [[nodiscard]] const ISource* getSource() const noexcept;
+};
+
+FileSource::FileSource(const std::string& path) : FileSource(std::filesystem::path(path)) {
+}
+
+FileSource::FileSource(const char* path) : FileSource(std::filesystem::path(path)) {
+}
+
+FileSource::FileSource(const std::filesystem::path& path) : size_(0) {
+    if(!std::filesystem::exists(path) || !std::filesystem::is_regular_file(path))
+        throw std::runtime_error("Invalid path: " + path.generic_string());
+
+    size_ = static_cast<uint64_t>(std::filesystem::file_size(path));
+
+    ifs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    ifs.open(path.generic_string());
+    if(!ifs.is_open())
+        throw std::runtime_error("Failed to open file");
+}
+
+FileSource::~FileSource() {
+    if(ifs.is_open())
+        ifs.close();
+}
+
+void FileSource::read(char* dst, int64_t len) {
+    ifs.read(dst, len);
+    if(ifs.bad())
+        throw std::runtime_error("Read fail");
+}
+
+void FileSource::peek(char* dst, int64_t len) const {
+    auto o = tell();
+    ifs.read(dst, len);
+    ifs.seekg(o, std::ios::beg);
+    if(ifs.bad())
+        throw std::runtime_error("Peek fail");
+}
+
+void FileSource::seek(int64_t offset) {
+    ifs.seekg(offset, std::ios::beg);
+}
+
+int64_t FileSource::tell() const noexcept {
+    return ifs.tellg();
+}
+
+int64_t FileSource::size() const noexcept {
+    return size_;
+}
+
+// Non owning constructor
+BufferSource::BufferSource(const char* data, int64_t data_size)
+: ownedBuffer(nullptr), buffer(data), bufferSize(data_size), cur(0) {
+}
+
+// Owning constructor
+BufferSource::BufferSource(std::unique_ptr<char[]> data, int64_t data_size)
+: ownedBuffer(std::move(data)), buffer(nullptr), bufferSize(data_size), cur(0) {
+    buffer = ownedBuffer.get();
+}
+
+void BufferSource::read(char* dst, int64_t len) {
+    if(cur + len > bufferSize)
+        throw std::runtime_error("Out of bounds read");
+    memcpy(dst, &(buffer[cur]), static_cast<size_t>(len));
+    cur += len;
+}
+
+void BufferSource::peek(char* dst, int64_t len) const {
+    if(cur + len > bufferSize)
+        throw std::runtime_error("Out of bounds read");
+    memcpy(dst, &(buffer[cur]), len);
+}
+
+void BufferSource::seek(int64_t offset) {
+    cur = offset; // Seeking to OOB is not an error
+}
+
+int64_t BufferSource::tell() const noexcept {
+    return cur;
+}
+
+int64_t BufferSource::size() const noexcept {
+    return bufferSize;
+}
+
+void reverseEndianness(char* data, size_t size) {
+    std::reverse(data, data + size);
+}
+
+template <size_t TypeSize>
+void reverseEndianness(char* data) {
+    std::reverse(data, data + TypeSize);
+}
+
+template <typename T>
+void reverseEndianness(T& t) {
+    static_assert(std::is_fundamental_v<T>);
+    std::reverse(reinterpret_cast<char*>(&t), reinterpret_cast<char*>(&t) + sizeof(T));
+}
+
+template <>
+void reverseEndianness<std::string>(std::string& str) {
+    std::reverse(str.begin(), str.end());
+}
+
+BinaryReader::BinaryReader(const std::filesystem::path& path) {
+    source = std::make_unique<FileSource>(path);
+}
+
+BinaryReader::BinaryReader(const std::string& path) {
+    source = std::make_unique<FileSource>(path);
+}
+
+BinaryReader::BinaryReader(const char* path) {
+    source = std::make_unique<FileSource>(path);
+}
+
+BinaryReader::BinaryReader(const char* data, int64_t data_size) {
+    source = std::make_unique<BufferSource>(data, data_size);
+}
+
+BinaryReader::BinaryReader(std::unique_ptr<char[]> data, int64_t data_size) {
+    source = std::make_unique<BufferSource>(std::move(data), data_size);
+}
+
+BinaryReader::BinaryReader(std::unique_ptr<ISource> source) : source(std::move(source)) {
+}
+
+int64_t BinaryReader::tell() const noexcept {
+    return source->tell();
+}
+
+void BinaryReader::seek(int64_t pos) {
+    source->seek(pos);
+}
+
+int64_t BinaryReader::size() const noexcept {
+    return source->size();
+}
+
+template <typename T, BinaryReader::Endianness en>
+void BinaryReader::read(T* arr, int64_t len) {
+    source->read(reinterpret_cast<char*>(arr), sizeof(T) * len);
+    if constexpr((en == Endianness::BE) && (sizeof(T) > sizeof(char))) {
+        for(int i = 0; i < len; ++i)
+            reverseEndianness(arr[i]);
+    }
+}
+
+template <typename T, BinaryReader::Endianness en>
+void BinaryReader::peek(T* arr, int64_t len) const {
+    source->peek(reinterpret_cast<char*>(arr), sizeof(T) * len);
+    if constexpr((en == Endianness::BE) && (sizeof(T) > sizeof(char))) {
+        for(int i = 0; i < len; ++i)
+            reverseEndianness(arr[i]);
+    }
+}
+
+template <typename T, BinaryReader::Endianness en>
+T BinaryReader::read() {
+    static_assert(std::is_trivially_copyable_v<T>);
+
+    if constexpr(en == BinaryReader::Endianness::BE)
+        static_assert(std::is_fundamental_v<T>);
+
+    T value;
+    read<T, en>(&value, 1);
+    return value;
+}
+
+template <typename T>
+void BinaryReader::sink(int64_t len) {
+    for(int i = 0; i < len; ++i)
+        static_cast<void>(read<T>());
+}
+
+template <typename T>
+void BinaryReader::sink() {
+    static_cast<void>(read<T>());
+}
+
+template <typename T, BinaryReader::Endianness en>
+T BinaryReader::peek() const {
+    static_assert(std::is_trivially_copyable_v<T>);
+
+    T value;
+    peek<T, en>(&value, 1);
+    return value;
+}
+
+template <unsigned int len, BinaryReader::Endianness en>
+std::string BinaryReader::readString() {
+    std::string str(len, '\0');
+    read(str.data(), len);
+    if constexpr(en == Endianness::LE)
+        reverseEndianness(str);
+
+    if(std::find(str.begin(), str.end(), '\0') != str.end())
+        throw std::runtime_error("Read fixed size string containing null characters");
+
+    return str;
+}
+
+template <BinaryReader::Endianness en>
+std::string BinaryReader::readString(size_t charCount) {
+    std::string str(charCount, '\0');
+    read(str.data(), charCount);
+    if constexpr(en == Endianness::LE)
+        reverseEndianness(str);
+    return str;
+}
+
+template <BinaryReader::Endianness en>
+std::string BinaryReader::readCString() {
+    std::vector<char> readBuffer;
+
+    char c = '\0';
+    do {
+        c = read<char>();
+        readBuffer.push_back(c);
+    } while(c != '\0');
+
+    std::string str(readBuffer.begin(), readBuffer.end() - 1);
+
+    if constexpr(en == Endianness::LE)
+        reverseEndianness(str);
+
+    return str;
+}
+
+template <unsigned int alignment>
+void BinaryReader::align() {
+    char zero[alignment];
+    uint64_t pos = tell();
+    uint64_t padding_len = (alignment - pos) % alignment;
+    read(zero, padding_len);
+}
+
+template <unsigned int alignment>
+void BinaryReader::alignZeroPad() {
+    char zero[alignment];
+    uint64_t pos = tell();
+    uint64_t padding_len = (alignment - pos) % alignment;
+    read(zero, padding_len);
+
+    auto it = std::find_if(std::begin(zero), &zero[padding_len], [](const char& c) { return c != 0; });
+
+    if(it != &zero[padding_len])
+        throw std::runtime_error("Non zero padding encountered");
+}
+
+const ISource* BinaryReader::getSource() const noexcept {
+    return source.get();
+}
+
+template <typename Source>
+template <typename... Args>
+CoverageTrackingSource<Source>::CoverageTrackingSource(Args&&... args)
+: Source(std::forward<Args>(args)...) {
+    accessPattern.resize(Source::size(), 0);
+}
+
+template <typename Source>
+void CoverageTrackingSource<Source>::read(char* dst, int64_t len) {
+    auto cur = Source::tell();
+    Source::read(dst, len);
+    for(auto i = cur; i < cur + len; ++i) {
+        if(accessPattern[i]++)
+            throw std::runtime_error("Double read");
+    }
+}
+
+template <typename Source>
+bool CoverageTrackingSource<Source>::completeCoverage(const BinaryReader* br) {
+    auto coverageReader = dynamic_cast<const CoverageTrackingSource<Source>*>(br->getSource());
+    if(!coverageReader)
+        throw std::bad_cast();
+    return coverageReader->completeCoverageInternal();
+}
+
+template <typename Source>
+bool CoverageTrackingSource<Source>::completeCoverageInternal() const {
+    auto it = std::find(accessPattern.begin(), accessPattern.end(), 0);
+    if(it == accessPattern.end())
+        return true;
+    return false;
+}
+
+} // namespace ZBinaryReader
---
+++ test/ZBinaryReaderTest.cpp
@@ -0,0 +1,381 @@
+#include "ZBinaryReader.hpp"
+#include "gtest/gtest.h"
+
+using namespace ZBinaryReader;
+
+namespace {
+
+template <class Dst>
+Dst safeCharArrayCast(const char* src) noexcept {
+    static_assert(std::is_trivially_copyable_v<Dst>);
+
+    Dst dst;
+    std::memcpy(&dst, src, sizeof(Dst));
+    return dst;
+}
+
+struct TriviallyCopyableStruct {
+    int a;
+    int b;
+};
+
+static_assert(std::is_trivially_copyable<TriviallyCopyableStruct>::value);
+
+bool operator==(const TriviallyCopyableStruct& s0, const TriviallyCopyableStruct& s1) {
+    return (s0.a == s1.a) && (s0.b == s1.b);
+}
+
+constexpr char testData[] = {
+    static_cast<char>(0x20), static_cast<char>(0xA0), static_cast<char>(0x24),
+    static_cast<char>(0x29), static_cast<char>(0xC3), static_cast<char>(0x18),
+    static_cast<char>(0xCF), static_cast<char>(0x28), static_cast<char>(0x23),
+    static_cast<char>(0x9F), static_cast<char>(0x24), static_cast<char>(0x29),
+    static_cast<char>(0xC3), static_cast<char>(0x18), static_cast<char>(0xFD),
+    static_cast<char>(0xBE), static_cast<char>(0x1F), static_cast<char>(0x85),
+    static_cast<char>(0xEB), static_cast<char>(0x51), static_cast<char>(0xB8),
+    static_cast<char>(0x1E), static_cast<char>(0x09), static_cast<char>(0x40),
+    static_cast<char>(0x7B), static_cast<char>(0x14), static_cast<char>(0x2E),
+    static_cast<char>(0x40), static_cast<char>(0x54), static_cast<char>(0x65),
+    static_cast<char>(0x73), static_cast<char>(0x74), static_cast<char>(0x74),
+    static_cast<char>(0x73), static_cast<char>(0x65), static_cast<char>(0x54),
+    static_cast<char>(0x54), static_cast<char>(0x65), static_cast<char>(0x73),
+    static_cast<char>(0x74), static_cast<char>(0x00), static_cast<char>(0x74),
+    static_cast<char>(0x73), static_cast<char>(0x65), static_cast<char>(0x54),
+    static_cast<char>(0x00), static_cast<char>(0x00), static_cast<char>(0x00),
+    static_cast<char>(0x00), static_cast<char>(0x00), static_cast<char>(0x00),
+    static_cast<char>(0x00), static_cast<char>(0x00), static_cast<char>(0x00),
+    static_cast<char>(0x00), static_cast<char>(0x00), static_cast<char>(0x00),
+    static_cast<char>(0x00), static_cast<char>(0x00), static_cast<char>(0x00),
+    static_cast<char>(0x00), static_cast<char>(0x00), static_cast<char>(0x54),
+    static_cast<char>(0x65), static_cast<char>(0x44)
+};
+
+std::filesystem::path getTmpTestDataFilePath() {
+    return std::filesystem::temp_directory_path() / "testData.bin";
+}
+
+template <typename Source>
+class BinaryReaderTestFixture : public testing::Test {
+protected:
+    static void SetUpTestSuite() {
+        if constexpr(std::is_same_v<Source, FileSource>) {
+            const auto path = getTmpTestDataFilePath();
+            if(path.empty())
+                throw std::runtime_error("Failed to contruct temp path for testData file");
+
+            std::ofstream ofs(path.generic_string(), std::ios::binary | std::ios::out);
+            ofs.exceptions(std::ios::failbit | std::ios::badbit);
+            if(!ofs.is_open())
+                throw std::runtime_error("Failed to open temp testData file");
+            ofs.write(testData, sizeof(testData));
+            ofs.close();
+        }
+    }
+
+    static void TeardownTestSuite() {
+        const auto path = getTmpTestDataFilePath();
+        if(!path.empty() && std::filesystem::exists(path))
+            std::filesystem::remove(path);
+    }
+
+    void SetUp() override {
+
+        if constexpr(std::is_same_v<Source, BufferSource>) {
+            br = std::make_unique<BinaryReader>(testData, sizeof(testData));
+        } else if constexpr(std::is_same_v<Source, FileSource>)
+            br = std::make_unique<BinaryReader>(getTmpTestDataFilePath());
+        else
+            throw; // unreachable;
+    }
+
+    template <typename ReadT>
+    void validatedTrivialLERead() {
+        const auto off = br->tell();
+        ASSERT_EQ(br->template read<ReadT>(), safeCharArrayCast<ReadT>(&testData[off]));
+        ASSERT_EQ(br->tell(), off + sizeof(ReadT));
+    }
+
+    template <typename ReadT>
+    void validatedTrivialBERead() {
+        const auto off = br->tell();
+
+        char bytes[sizeof(ReadT)];
+        std::memcpy(bytes, &testData[off], sizeof(ReadT));
+        std::reverse(std::begin(bytes), std::end(bytes));
+
+        ASSERT_EQ((br->template read<ReadT, BinaryReader::Endianness::BE>()), safeCharArrayCast<ReadT>(bytes));
+        ASSERT_EQ(br->tell(), off + sizeof(ReadT));
+    }
+
+    template <typename ReadT, int64_t len>
+    void validatedLEArrayRead() {
+        const auto off = br->tell();
+
+        ReadT arr[len];
+        br->read(arr, len);
+        for(int i = 0; i < len; ++i)
+            ASSERT_EQ(arr[i], safeCharArrayCast<ReadT>(&testData[off + sizeof(ReadT) * i]));
+
+        ASSERT_EQ(br->tell(), off + sizeof(ReadT) * len);
+    }
+
+
+    template <typename ReadT, int64_t len>
+    void validatedBEArrayRead() {
+        const auto off = br->tell();
+
+        ReadT arr[len];
+        br->read<ReadT, BinaryReader::Endianness::BE>(arr, len);
+        for(int i = 0; i < len; ++i) {
+            ReadT val = safeCharArrayCast<ReadT>(&testData[off + sizeof(ReadT) * i]);
+            std::reverse(reinterpret_cast<char*>(&val), reinterpret_cast<char*>(&val) + sizeof(ReadT));
+            ASSERT_EQ(arr[i], val);
+        }
+
+        ASSERT_EQ(br->tell(), off + sizeof(ReadT) * len);
+    }
+
+    void validatedSeekTell(int seekOffset) {
+        br->seek(seekOffset);
+        ASSERT_EQ(br->tell(), seekOffset);
+    }
+
+    std::unique_ptr<BinaryReader> br;
+};
+
+#if GTEST_HAS_TYPED_TEST
+
+using testing::Types;
+
+typedef Types<FileSource, BufferSource> Implementations;
+
+TYPED_TEST_SUITE(BinaryReaderTestFixture, Implementations);
+
+TYPED_TEST(BinaryReaderTestFixture, SourceSize) {
+    ASSERT_EQ(this->br->size(), sizeof(testData));
+}
+
+TYPED_TEST(BinaryReaderTestFixture, SeekTell) {
+    // Seek 0->0
+    this->validatedSeekTell(0);
+
+    // Seek to valid position.
+    this->validatedSeekTell(10);
+
+    // Seek to end
+    this->validatedSeekTell(sizeof(testData));
+
+    // Seek past end
+    this->validatedSeekTell(sizeof(testData) + 1);
+
+    // TODO: Define behaviour for seeking negative values
+    // seek to neg offset
+    // br.seek(-1);
+    // ASSERT_EQ(br.tell(), 0);
+}
+
+TYPED_TEST(BinaryReaderTestFixture, ReadPrimitiveTypes) {
+    this->br->seek(0);
+
+    this->template validatedTrivialLERead<int64_t>();
+    this->template validatedTrivialLERead<int32_t>();
+    this->template validatedTrivialLERead<int16_t>();
+    this->template validatedTrivialLERead<int8_t>();
+    this->template validatedTrivialLERead<uint64_t>();
+    this->template validatedTrivialLERead<uint32_t>();
+    this->template validatedTrivialLERead<uint16_t>();
+    this->template validatedTrivialLERead<uint8_t>();
+    this->template validatedTrivialLERead<double>();
+    this->template validatedTrivialLERead<float>();
+
+    this->br->seek(0);
+    this->template validatedTrivialBERead<int64_t>();
+    this->template validatedTrivialBERead<int32_t>();
+    this->template validatedTrivialBERead<int16_t>();
+    this->template validatedTrivialBERead<int8_t>();
+    this->template validatedTrivialBERead<uint64_t>();
+    this->template validatedTrivialBERead<uint32_t>();
+    this->template validatedTrivialBERead<uint16_t>();
+    this->template validatedTrivialBERead<uint8_t>();
+    this->template validatedTrivialBERead<double>();
+    this->template validatedTrivialBERead<float>();
+}
+
+TYPED_TEST(BinaryReaderTestFixture, ReadArrays) {
+    this->br->seek(0);
+    this->template validatedLEArrayRead<int64_t, 2>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<int32_t, 4>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<int16_t, 8>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<int8_t, 16>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<uint64_t, 2>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<uint32_t, 4>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<uint16_t, 8>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<uint8_t, 16>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<double, 2>();
+    this->br->seek(0);
+    this->template validatedLEArrayRead<float, 4>();
+
+    this->br->seek(0);
+    this->template validatedBEArrayRead<int64_t, 2>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<int32_t, 4>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<int16_t, 8>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<int8_t, 16>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<uint64_t, 2>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<uint32_t, 4>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<uint16_t, 8>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<uint8_t, 16>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<double, 2>();
+    this->br->seek(0);
+    this->template validatedBEArrayRead<float, 4>();
+}
+
+TYPED_TEST(BinaryReaderTestFixture, ReadLEArrayOfStruct) {
+    this->br->seek(0);
+    this->template validatedLEArrayRead<TriviallyCopyableStruct, 2>();
+}
+
+TYPED_TEST(BinaryReaderTestFixture, ReadStrings) {
+    this->br->seek(0x1C);
+
+    ASSERT_STREQ(this->br->template readString<4>().c_str(), "Test");
+    ASSERT_STREQ((this->br->template readString<4, BinaryReader::Endianness::LE>().c_str()),
+                 "Test");
+    ASSERT_STREQ(this->br->readCString().c_str(), "Test");
+    ASSERT_STREQ(this->br->template readCString<BinaryReader::Endianness::LE>().c_str(), "Test");
+}
+
+// Try to read fixed size string which contains null chars
+TYPED_TEST(BinaryReaderTestFixture, ReadInvalidString) {
+    this->br->seek(0x20);
+    ASSERT_THROW(this->br->template readString<0x0A>(), std::runtime_error);
+}
+
+TYPED_TEST(BinaryReaderTestFixture, ReadStringPastEnd) {
+    this->br->seek(0x3E);
+    ASSERT_THROW(this->br->template readString<0x10>(), std::runtime_error);
+}
+
+TYPED_TEST(BinaryReaderTestFixture, ReadCStringPastEnd) {
+    this->br->seek(0x3E);
+    ASSERT_THROW(this->br->readCString(), std::runtime_error);
+}
+
+TYPED_TEST(BinaryReaderTestFixture, ReadCopyableType) {
+    this->template validatedTrivialLERead<TriviallyCopyableStruct>();
+}
+
+TYPED_TEST(BinaryReaderTestFixture, Alignment) {
+    // Align zero
+    this->br->align();
+    ASSERT_EQ(this->br->tell(), 0);
+
+    // Align already aligned
+    this->br->seek(1);
+    this->br->template align<1>();
+    ASSERT_EQ(this->br->tell(), 1);
+
+    // Align from -1 off
+    this->br->align();
+    ASSERT_EQ(this->br->tell(), 0x10);
+
+    // Align from 1 off
+    this->br->template align<0x11>();
+    ASSERT_EQ(this->br->tell(), 0x11);
+
+    // Align to multiple
+    this->br->template align<4>();
+    ASSERT_EQ(this->br->tell(), 0x14);
+}
+
+// Throw if aligning past end of read source
+TYPED_TEST(BinaryReaderTestFixture, AlignmentPastEnd) {
+    this->br->seek(1);
+    ASSERT_THROW(this->br->template align<sizeof(testData) + 1>(), std::runtime_error);
+}
+
+TYPED_TEST(BinaryReaderTestFixture, AlignmentZeroPad) {
+    // Align already aligned
+    this->br->alignZeroPad();
+    ASSERT_EQ(this->br->tell(), 0);
+
+    // Align from 'Alignment'-1 off
+    this->br->seek(0x31);
+    this->br->template alignZeroPad<8>();
+    ASSERT_EQ(this->br->tell(), 0x38);
+
+    // Align from 1 off
+    this->br->seek(0x2F);
+    this->br->alignZeroPad();
+    ASSERT_EQ(this->br->tell(), 0x30);
+}
+
+// Throw if aligning past end of read source
+TYPED_TEST(BinaryReaderTestFixture, AlignmentZeroPadPastEnd) {
+    this->br->seek(1);
+    ASSERT_THROW(this->br->template alignZeroPad<sizeof(testData) + 1>(),
+                 std::runtime_error); // Will throw before checking padding.
+}
+
+// Throw if padding contains non-zero values.
+TYPED_TEST(BinaryReaderTestFixture, AlignmentZeroPadBadPadding) {
+    this->br->seek(1);
+    ASSERT_THROW(this->br->alignZeroPad(), std::runtime_error);
+}
+
+#endif // GTEST_HAS_TYPED_TEST
+
+
+class CoverageTrackingSourceTestFixture : public testing::Test {
+protected:
+    void SetUp() override {
+        std::unique_ptr<BufferSource> source =
+        std::make_unique<CoverageTrackingSource<BufferSource>>(testData, sizeof(testData));
+        br = std::make_unique<BinaryReader>(std::move(source));
+    }
+
+    std::unique_ptr<BinaryReader> br;
+};
+
+TEST_F(CoverageTrackingSourceTestFixture, CompleteCoverage) {
+    char sink[sizeof(testData)];
+    br->read(sink, sizeof(sink));
+    ASSERT_TRUE(CoverageTrackingSource<BufferSource>::completeCoverage(br.get()));
+}
+
+TEST_F(CoverageTrackingSourceTestFixture, IncompleteCoverage) {
+    char sink[sizeof(testData) - 1];
+    br->read(sink, sizeof(sink));
+    ASSERT_FALSE(CoverageTrackingSource<BufferSource>::completeCoverage(br.get()));
+}
+
+TEST_F(CoverageTrackingSourceTestFixture, DoubleRead) {
+    using T = int;
+    br->read<T>();
+    br->seek(br->tell() - sizeof(T));
+    ASSERT_THROW(br->read<T>(), std::runtime_error);
+}
+
+TEST_F(CoverageTrackingSourceTestFixture, DoublePeek) {
+    using T = int;
+    br->peek<T>();
+    br->seek(br->tell() - sizeof(T));
+    br->peek<T>();
+}
+
+} // namespace
